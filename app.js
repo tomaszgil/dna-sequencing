79ed6785bc6ca28e807391159346df6e97128b94
Tomasz Gil <tomasz.gil@akai.org.pl>
2018-04-17 10:59:40 +0200

    Initialize algorithm with greedy solution

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const GeneticSolver = require('./genetic/GeneticSolver');
 
 const data = new Data('data/9.200-40.txt');
 const gs = new GeneticSolver({
-  populationSize: 300,
-  maxGeneration: 1000,
-  mutationRate: 0.03,
-  fitnessGrowth: 1
+  populationSize: 500,
+  maxGeneration: 2000,
+  mutationRate: 0.05,
+  fitnessGrowth: 3
 });
 
 gs.solve(data);
